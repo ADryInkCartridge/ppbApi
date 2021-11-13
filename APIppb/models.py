248bba1ea89f6d7e9c8d3daa5e66517f2e86decf
Bayu Adjie Sidharta <31591861+ADryInkCartridge@users.noreply.github.com>
2021-11-13 17:23:11 +0700

    update pw field

--- APIppb/models.py
+++ APIppb/models.py
@@ -5,5 +5,6 @@ from django.db import models
 class Images(models.Model):
     filename = models.TextField()
     imagedata = models.TextField()
+    password = models.TextField()
     def __str__(self):
         return self.filename
--- APIppb/serializers.py
+++ APIppb/serializers.py
@@ -6,4 +6,4 @@ from .models import Images
 class ImagesSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Images
-        fields = ('id','filename', 'imagedata')
+        fields = ('id','filename', 'imagedata','password')
