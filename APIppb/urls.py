47fcfbd2d41aa948abae368548d5f7fcd695ff79
Ryan Garnet Andrianto <ryangarnetandrianto@gmail.com>
2021-11-15 18:41:42 +0700

    New web page test

--- APIppb/urls.py
+++ APIppb/urls.py
@@ -10,5 +10,6 @@ router.register(r'images', views.ImageViewSet)
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
     path('', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('my-images/', views.viewImages)
 ]
--- APIppb/views.py
+++ APIppb/views.py
@@ -6,8 +6,13 @@ from rest_framework import viewsets
 
 from .serializers import ImagesSerializer
 from .models import Images
+from django.http import HttpResponse
 
 
 class ImageViewSet(viewsets.ModelViewSet):
     queryset = Images.objects.all().order_by('filename')
     serializer_class = ImagesSerializer
+
+def viewImages(request):
+    # images = Images.objects.all()
+    return HttpResponse("ehe")
