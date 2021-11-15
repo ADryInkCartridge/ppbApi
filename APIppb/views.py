2726bbc1645cd93a01f7aeb0a58fb5dda0754e88
Bayu Adjie Sidharta <31591861+ADryInkCartridge@users.noreply.github.com>
2021-11-15 20:55:12 +0700

    website

--- APIppb/views.py
+++ APIppb/views.py
@@ -1,3 +1,4 @@
+from django.http.response import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
@@ -6,8 +7,17 @@ from rest_framework import viewsets
 
 from .serializers import ImagesSerializer
 from .models import Images
+from django.template import loader
 
 
 class ImageViewSet(viewsets.ModelViewSet):
     queryset = Images.objects.all().order_by('filename')
     serializer_class = ImagesSerializer
+
+def ViewImages(request):
+    images = Images.objects.all().order_by('filename')
+    template = loader.get_template('./images.html')
+    context = {
+        'images': images,
+    }
+    return HttpResponse(template.render(context, request))
--- testing/settings.py
+++ testing/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-6mhjqa)zurh-efrwz!(mgt)h3+k0y+dpbq8#j=gfein4xigbdo
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'testing.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
